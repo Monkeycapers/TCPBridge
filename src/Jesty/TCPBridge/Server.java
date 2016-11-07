b7a5ce9fbae5396e3500ae6adeb3ab33f9c9119a
Evan <evanjesty@gmail.com>
2016-11-07 00:54:45 -0500

    Init commit

---
+++ src/Jesty/TCPBridge/Server.java
@@ -0,0 +1,47 @@
+package Jesty.TCPBridge;
+
+import java.util.ArrayList;
+
+/**
+ * Created by S199753733 on 10/19/2016.
+ *
+ *
+ */
+public class Server {
+
+    protected Clients clients;
+
+    int raw_port, web_port;
+
+    public Server(int raw_port, int web_port) {
+        this.web_port = web_port;
+        this.raw_port = raw_port;
+    }
+
+    public Server() {
+        raw_port = 16000; web_port = 8080;
+    }
+
+
+    public void start() {
+        clients = new Clients(this);
+        ClientFactory clientFactory = new ClientFactory(clients, raw_port);
+        WebFactory webFactory = new WebFactory(clients, web_port);
+        webFactory.start();
+        new Thread(clientFactory).start();
+    }
+
+    public void onMessage(ClientWorker clientWorker, String message) {
+        System.out.println(clientWorker.id + " " + message);
+        for (ClientWorker client: clients.getList()) {
+            client.sendMessage(message);
+        }
+    }
+    public void onClose(ClientWorker clientWorker, int code) {
+        clients.getList().remove(clientWorker);
+    }
+    public void onOpen(ClientWorker clientWorker, int code) {
+
+    }
+
+}
---
+++ src/Jesty/TCPBridge/SocketType.java
@@ -0,0 +1,10 @@
+package Jesty.TCPBridge;
+
+/**
+ * Created by Evan on 10/19/2016.
+ */
+public enum SocketType {
+
+    WEB_SOCKET, RAW_SOCKET
+
+}
